043c1c0c2096f1a78e07a5943f8420209120c9cc
Nicholas Aresta Ngadiman <nicharesta.ng@gmail.com>
2025-02-18 13:36:30 +0000

    Second iteration of website. Made it clean and minimalistic. Minimal Javascript, mostly CSS. Added more details about experience. Projects unimplemented.

--- js/script.js
+++ js/script.js
@@ -1,23 +1,10 @@
-const moonIcon = document.getElementById("moon-icon");
-const body = document.body;
-
-const savedTheme = localStorage.getItem("theme");
-if (savedTheme === "light") {
-    body.classList.add("light-mode");
-    moonIcon.classList.replace("fa-moon", "fa-sun");
-    document.getElementById("addepar_logo").src = "images/addepar_logo_light.jpeg";
-    document.getElementById("rareskills_io_logo").src = "images/rareskills_io_logo_light.jpeg";
-}
-
-moonIcon.addEventListener("click", () => {
-    body.classList.toggle("light-mode");
-    if (body.classList.contains("light-mode")) {
-        moonIcon.classList.replace("fa-moon", "fa-sun");
-        document.getElementById("addepar_logo").src = "images/addepar_logo_light.jpeg";
-        localStorage.setItem("theme", "light");
-    } else {
-        document.getElementById("addepar_logo").src = "images/addepar_logo_dark.png";
-        moonIcon.classList.replace("fa-sun", "fa-moon");
-        document.getElementById("rareskills_io_logo").src = "images/rareskills_io_logo_dark.png";
-    }
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("copyright-year").textContent = new Date().getFullYear();
 });
+
+function replayAnimation() {
+    const element = document.querySelector('.wave');
+    element.classList.remove('wave');
+    void element.offsetWidth;
+    element.classList.add('wave');
+}
